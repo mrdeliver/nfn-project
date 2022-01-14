7d5702706c3d589573cf7a6112918f19a200ddb4
Moritz Spranger <moritz.spranger@gmail.com>
2022-01-15 00:50:40 +0100

    API test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,17 @@
 <template>
+<h1>LEADERBOARD BLA BLA ASSETS NFT COLLECTION BLA</h1>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div v-for="asset in assets" :key="asset" class="asset">
+      <img :src="asset.image_url" :alt="asset.asset_contract.name">
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.opensea.io/api/v1/';
 
 @Options({
   props: {
@@ -43,23 +20,51 @@ import { Options, Vue } from 'vue-class-component';
 })
 export default class HelloWorld extends Vue {
   msg!: string
+
+  private collection: any = null
+
+  private primaryAssetContracts: any = null
+
+  private assets: any = null
+
+  async fetchCollection(name: string): Promise<void> {
+    await axios
+      .get(`collection/${name}`)
+      .then((response) => {
+        this.collection = response.data.collection;
+        this.primaryAssetContracts = this.collection.primary_asset_contracts;
+      });
+  }
+
+  async fetchAssets(contract:string): Promise<void> {
+    await axios
+      .get(`assets?asset_contract_address=${contract}&order_direction=desc&offset=0&limit=20`)
+      .then((response) => {
+        console.log(response);
+        this.assets = response.data.assets;
+      });
+  }
+
+  async mounted(): Promise<void> {
+    await this.fetchCollection('boredapeyachtclub');
+    await this.fetchAssets(this.primaryAssetContracts[0].address);
+    console.log(this.assets);
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+
+.asset {
+  width: 20%;
+  float: left;
+  padding: 2%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+img {
+  max-width:100%;
+  max-height:100%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
