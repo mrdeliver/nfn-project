e26070ad23f76272ac4b682d37dc7d96c7c25a96
simonstadlinger <stadlinger.simon@gmail.com>
2022-03-31 11:10:14 +0200

    Added three icons

---
+++ src/components/primitives/icons/IconLock.vue
@@ -0,0 +1,27 @@
+<!-- eslint-disable max-len -->
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42.59 42.6"
+    :width="iconSize" :height="iconSize"
+      >
+      <g :fill="iconColor">
+        <path id="e" class="h" d="M0,13.83v27.71c0,.58,.46,1.05,1.04,1.05,0,0,0,0,.01,0H13.84c.58,0,1.05-.46,1.05-1.04,0,0,0,0,0-.01V13.83c0-.58-.46-1.05-1.04-1.05,0,0,0,0-.01,0H1.05C.47,12.77,0,13.24,0,13.81c0,0,0,0,0,.01Z"/><path id="f" class="h" d="M18.08,0c-.58,0-1.05,.46-1.05,1.04,0,0,0,0,0,.01V42.58h15.98c.58,0,1.05-.46,1.05-1.04,0,0,0,0,0-.01V1.05C34.07,.47,33.6,0,33.03,0c0,0,0,0-.01,0h-14.93Z"/><path id="g" class="h" d="M50.05,21.29h-12.8c-.58,0-1.05,.46-1.05,1.04,0,0,0,0,0,.01v19.19c0,.58,.46,1.05,1.04,1.05,0,0,0,0,.01,0h12.8c.58,0,1.05-.46,1.05-1.04,0,0,0,0,0-.01V22.34c0-.58-.46-1.05-1.04-1.05,0,0,0,0-.01,0Z"/>
+      </g>
+    </svg>
+</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+
+@Options({
+  name: 'IconLock',
+})
+export default class IconLock extends Vue {
+  @Prop({ type: Number })
+  iconSize = 18;
+
+  @Prop({ type: String })
+  iconColor = 'currentColor';
+}
+
+</script>
---
+++ src/components/primitives/icons/IconQuestionmarc.vue
@@ -0,0 +1,27 @@
+<!-- eslint-disable max-len -->
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42.59 42.6"
+    :width="iconSize" :height="iconSize"
+      >
+      <g :fill="iconColor">
+        <path id="d" class="e" d="M33.22,18.6h-1.99v-5.98C31.23,5.65,25.58,0,18.6,0S5.98,5.65,5.98,12.62v5.98h-1.99C1.79,18.6,0,20.39,0,22.59v15.95c0,2.2,1.79,3.99,3.99,3.99h29.23c2.2,0,3.99-1.78,3.99-3.99v-15.95c0-2.2-1.79-3.99-3.99-3.99Zm-8.64,0H12.62v-5.98c0-3.3,2.68-5.98,5.98-5.98,3.3,0,5.98,2.68,5.98,5.98v5.98Z"/>
+      </g>
+    </svg>
+</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+
+@Options({
+  name: 'IconQuestionmarc',
+})
+export default class IconQuestionmarc extends Vue {
+  @Prop({ type: Number })
+  iconSize = 18;
+
+  @Prop({ type: String })
+  iconColor = 'currentColor';
+}
+
+</script>
